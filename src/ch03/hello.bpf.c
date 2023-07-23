b4334f182512ff4329c7406e8fc03d21b6d1bf8b
sat0ken <15720506+sat0ken@users.noreply.github.com>
2023-07-23 19:54:05 +0900

    update

--- src/ch03/hello.bpf.c
+++ src/ch03/hello.bpf.c
@@ -1,13 +1,13 @@
-#include <linux/bpf.h>
+#include <linux/bpf.h>  // 1.
 #include <bpf/bpf_helpers.h>
 
-int counter = 0;
+int counter = 0;    // 2.
 
-SEC("xdp")
-int hello(struct xdp_md *ctx) {
+SEC("xdp")          // 3.
+int hello(struct xdp_md *ctx) {     // 4.
     bpf_printk("Hello World %d", counter);
     counter++;
     return XDP_PASS;
 }
 
-char LICENSE[] SEC("license") = "Dual BSD/GPL";
+char LICENSE[] SEC("license") = "Dual BSD/GPL";     // 5.
